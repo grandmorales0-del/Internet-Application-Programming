8b189b76f7d2e211fbba7cceb0576a0675ad2b7e
grandmorales0-del <grandmorales.0@gmail.com>
2026-09-15 22:32:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.test-drive-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        const carName = this.parentElement.getAttribute('data-car');
-        alert(`Thank you for your interest! A representative will contact you shortly to schedule your test drive for the ${carName}.`);
-    });
-});
